689aa4a2780cd0809b05dd2cacc79301584b56fa
Shwetha Chatra Raveesh <60267691+shwethacr1@users.noreply.github.com>
2020-10-23 01:32:52 -0400

    Update uniqueCharaters.js

--- uniqueCharaters.js
+++ uniqueCharaters.js
@@ -1,1 +1,15 @@
-# ctciJavaScript
+var name = 'enterastring'
+function checkforunique(){
+  var toArray = name.split('')
+  toArray = toArray.sort()
+  for(var i=0;i<toArray.length;i++){
+    if(toArray[i] === toArray[i+1]) {
+      return false
+    }
+    else {
+      continue
+    }
+  }
+  return true
+
+}
